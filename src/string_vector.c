5141b780030be40362fef1224bfaac1c06f4222d
antiphy <antiphycz@gmail.com>
2023-10-21 17:15:02 +0200

    Added vector support

---
+++ src/string_vector.c
@@ -0,0 +1,83 @@
+/**
+ * @file string_vector.c
+ * 
+ * IFJ compiler 2023
+ * 
+ * @brief String vector file 
+ * @author Samuel Hejnicek
+ * @date 2023-10-21
+ */
+
+#include "string_vector.h"
+
+vector* vector_init(){
+
+    vector* v = malloc(sizeof(vector));
+    if(!(v)){
+        return NULL;
+    }
+
+    v->size = 0;
+    v->size_of_alloc = VECTOR_LENGTH;
+    v->array = (char* ) malloc(VECTOR_LENGTH* (sizeof(char)));
+
+    if(!(v->array)){
+        free(v);
+        return NULL;
+    }
+
+    v->array[0] = '\0';
+
+    return v;
+}
+
+bool vector_append(vector* v, char c){
+    if(v->size + 1 == v->size_of_alloc){
+        char *new_vec = realloc(v->array, v->size_of_alloc * 2 * sizeof(char));
+
+        if(!(new_vec)){
+            return false;
+        }
+
+        v->array = new_vec;
+        v->size_of_alloc *= 2;
+    }
+
+    v->array[v->size] = c;
+    v->size++;
+    v->array[v->size] = '\0';
+    return true;
+}
+
+bool vector_str_append(vector* v, char* c){
+    for(unsigned int i = 0; i < strlen(c); i++){
+        vector_append(v, c[i]);
+    }
+    return true;
+}
+
+void vector_clear(vector* v){
+    v->size = 0;
+    v->array[0] = '\0';
+}
+
+void vector_dispose(vector* v){
+    v->size = 0;
+    v->size_of_alloc = 0;
+    free(v->array);
+    v->array = NULL;
+    free(v);
+    v = NULL;
+}
+
+bool vector_str_cmp(vector* v, char* s){
+    return !(strcmp(v->array, s));
+}
+
+int vector_size(vector* v){
+    return v->size;
+}
+
+int vector_alloc_size(vector* v){
+    return v->size_of_alloc;
+}
---
+++ src/string_vector.h
@@ -0,0 +1,91 @@
+/**
+ * @file string_vector.c
+ * 
+ * IFJ compiler 2023
+ * 
+ * @brief String vector header file 
+ * @author Samuel Hejnicek
+ * @date 2023-10-21
+ */
+#ifndef STRING_VECTOR_H
+#define STRING_VECTOR_H
+
+
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+#define VECTOR_LENGTH 8
+
+typedef struct string_vector {
+    char* array; //array of strings
+    int size; //size of the vector
+    int size_of_alloc; //allocation size
+} vector;
+
+/**
+ * @brief inicialization of the vector
+ * 
+ * @return pointer to allocated vector
+ */
+vector* vector_init();
+
+/**
+ * @brief Adds char to the  array
+ * 
+ * @param v pointer to vector
+ * @param c char to be added
+ * @return true if done correctly, false otherwise 
+ */
+bool vector_append(vector* v, char c);
+
+/**
+ * @brief Adds string to the end of the actual vector
+ * 
+ * @param v pointer to vector
+ * @param s pointer to string to be added
+ * @return true if done correctly, false otherwise 
+ */
+bool vector_str_append(vector *v, char *s);
+
+/**
+ * @brief Clears vector elements
+ * 
+ * @param v pointer to vector
+ */
+void vector_clear(vector* v);
+
+/**
+ * @brief Free vector
+ * 
+ * @param v pointer to vector
+ */
+void vector_dispose(vector *v);
+
+/**
+ * @brief Compares vector and string
+ * 
+ * @param v1 pointer to 1st vector
+ * @param v2 pointer to string to be compared
+ * @return true if vectors match, false otherwise
+ */
+bool vector_str_cmp(vector* v1, char* s);
+
+/**
+ * @brief Return size of vector
+ * 
+ * @param v pointer to vector
+ * @return size of vector
+ */
+int vector_size(vector *v);
+
+/**
+ * @brief Return size of allocated space in vector
+ * 
+ * @param v pointer to vector
+ * @return size of allocated space for chars in vector
+ */
+int vector_alloc_size(vector *v);
+#endif
