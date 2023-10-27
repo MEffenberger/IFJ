7eaf4ef2d76dfc061eac3f996aae0d9a51132d7d
MarekEffenberger <marekeff@seznam.cz>
2023-10-27 18:00:46 +0200

    Subject to change, the pointer to the allocated symtables representing each scope in the input file

---
+++ src/forest.h
@@ -0,0 +1,40 @@
+/**
+ * @file forest.c
+ *
+ * IFJ23 compiler
+ *
+ * @brief A structure to better represent the hierarchy between the nodes of symtable and ast
+ *
+ * @author Marek Effenberger <xeffen00>
+ * @author Adam Valík <xvalik05>
+ */
+
+#ifndef IFJ_FOREST_H
+#define IFJ_FOREST_H
+
+#include "symtable.h"
+#include "error.h"
+
+typedef struct s_forest_node {
+    struct s_forest_node *parent;
+    struct s_forest_node **children;
+    AVL_tree *symtable;
+    // add key to represent the node's type
+} forest_node;
+
+void forest_init(forest_node **root);
+
+void forest_dispose(forest_node *root);
+
+void forest_insert_first(forest_node **forest);
+
+forest_node *forest_get_child(forest_node *parent, int index);
+
+forest_node *forest_get_parent(forest_node *node);
+
+AVL_tree *forest_get_symtable(forest_node *node);
+
+
+
+
+#endif //IFJ_FOREST_H
