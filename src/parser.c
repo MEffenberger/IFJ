bbe29dffd04461f98eca43e7d07508ac76aefa59
adamvalik <xvalik05@stud.fit.vutbr.cz>
2023-12-05 19:22:19 +0100

    hupsik dupsik

--- src/parser.c
+++ src/parser.c
@@ -1160,7 +1160,7 @@ void condition() {
 
     cnt_push(cnt_stack); // push ifelse_cnt to stack and increment
     sprintf(node_name, "if_%d", ifelse_cnt);
-    char *node_name2 = allocate_memory(sizeof(char) * 20);
+    char *node_name2 = (char*)allocate_memory(sizeof(char) * 20);
     strcpy(node_name2, node_name);
 
     MAKE_CHILDREN_IN_FOREST(W_IF, node_name2);
@@ -1246,7 +1246,7 @@ void condition() {
 
                 cnt = cnt_top(cnt_stack); // get ifelse_cnt from stack
                 sprintf(node_name, "else_%d", cnt);
-                char *node_name3 = allocate_memory(sizeof(char) * 20);
+                char *node_name3 = (char*)allocate_memory(sizeof(char) * 20);
                 strcpy(node_name3, node_name);
                 MAKE_CHILDREN_IN_FOREST(W_ELSE, node_name3);
                 active->cond_cnt = cnt;
@@ -1281,10 +1281,6 @@ void condition() {
                         BACK_TO_PARENT_IN_FOREST;
 
                         printf("-- returning...\n\n");
-                        free(node_name2);
-                        free(node_name3);
-                        node_name2 = NULL;
-                        node_name3 = NULL;
                         return;
                     }
                     else {
@@ -1316,7 +1312,7 @@ void cycle() {
     print_debug(current_token, 2, debug_cnt);
 
     sprintf(node_name, "while_%d", while_cnt);
-    char *node_name1 = allocate_memory(sizeof(char) * 20);
+    char *node_name1 = (char*)allocate_memory(sizeof(char) * 20);
     strcpy(node_name1, node_name);
   
     while_cnt++;
@@ -1373,7 +1369,7 @@ void cycle() {
             print_debug(current_token, 1, debug_cnt++);
 
             printf("-- returning...\n\n");
-            free(node_name1);
+
             return;
         }
         else {
