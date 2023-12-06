a65e90a1ddd421e87f79d4f5da25956ccbc1c972
PanHorut <xhorut01@vutbr.cz>
2023-12-06 14:09:34 +0100

    fixed comments in token_stack

--- src/token_stack.c
+++ src/token_stack.c
@@ -22,9 +22,11 @@
 #include "symtable.h"
 #include "token_stack.h"
 
+//default stack size
 #define STACK_SIZE 4
 
 void stack_init(token_stack* token_stack) {
+    
     token_stack->token_array = (token_t**) malloc(STACK_SIZE * sizeof(token_t*));
     token_stack->size = 0;
     token_stack->capacity = STACK_SIZE;
@@ -33,6 +35,7 @@ void stack_init(token_stack* token_stack) {
 
 void stack_push(token_stack* token_stack, token_t* token) {
 
+    // Not enough space
     if (token_stack->size == token_stack->capacity-1) {
 
         token_stack->capacity *= 2;
@@ -43,17 +46,24 @@ void stack_push(token_stack* token_stack, token_t* token) {
 }
 
 void stack_pop(token_stack* token_stack) {
+
     token_stack->size--;
 }
 
 token_t* stack_top(token_stack* token_stack) {
+
     return token_stack->token_array[token_stack->size-1];
 }
 
 
 token_t* stack_top_terminal(token_stack* token_stack){
+
     int counter = 0;
+
+    // current token on top
     token_t* tmp = token_stack->token_array[token_stack->size - 1 + counter];
+
+    // Iterating until we find the top terminal
     while(tmp->type == TOKEN_EXPRESSION){
         counter--;
         tmp = token_stack->token_array[token_stack->size -1 + counter];
@@ -62,12 +72,12 @@ token_t* stack_top_terminal(token_stack* token_stack){
 }
 
 void dispose_stack(token_stack* token_stack) {
+
     for(int i = 0; i < token_stack->size; i++){
         destroy_token(token_stack->token_array[i]);
     }
 
     free(token_stack->token_array);
     token_stack->token_array = NULL;
-    //free(token_stack);
-    //token_stack = NULL;
+    
 }
